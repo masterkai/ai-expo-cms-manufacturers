c6afa5aaf318cf32edd3e57ba4ad878860a61369
max liu <max.liu@digitimes.com>
2025-11-27 17:55:20 +0800

    feat: implement basic information and contact person components with form inputs and styles

--- src/app/home/basic-information/basic-information.ts
+++ src/app/home/basic-information/basic-information.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
+import { InputGroup } from "primeng/inputgroup";
+import { InputText } from "primeng/inputtext";
+import { InputGroupAddon } from "primeng/inputgroupaddon";
+import { FloatLabel } from "primeng/floatlabel";
 
 @Component({
-  selector: 'app-basic-information',
-  imports: [],
-  templateUrl: './basic-information.html',
-  styleUrl: './basic-information.scss'
+	selector: 'app-basic-information',
+	imports: [
+		Button,
+		InputGroup,
+		InputText,
+		InputGroupAddon,
+		FloatLabel
+	],
+	templateUrl: './basic-information.html',
+	styleUrl: './basic-information.scss'
 })
 export class BasicInformation {
 
--- src/app/home/contact-person/contact-person.ts
+++ src/app/home/contact-person/contact-person.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FloatLabel } from "primeng/floatlabel";
+import { InputText } from "primeng/inputtext";
+import { Button } from "primeng/button";
 
 @Component({
-  selector: 'app-contact-person',
-  imports: [],
-  templateUrl: './contact-person.html',
-  styleUrl: './contact-person.scss'
+	selector: 'app-contact-person',
+	imports: [
+		FloatLabel,
+		InputText,
+		Button
+	],
+	templateUrl: './contact-person.html',
+	styleUrl: './contact-person.scss'
 })
 export class ContactPerson {
 
