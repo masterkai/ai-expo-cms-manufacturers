97d18195c96dbae426f645cd611bc00cfa80b6fb
max liu <max.liu@digitimes.com>
2025-11-28 14:56:30 +0800

    feat: enhance contact person component with display and edit functionality

--- src/app/home/contact-person/contact-person.ts
+++ src/app/home/contact-person/contact-person.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
-import { FloatLabel } from "primeng/floatlabel";
-import { InputText } from "primeng/inputtext";
+import { Component, signal } from '@angular/core';
 import { Button } from "primeng/button";
 
 @Component({
 	selector: 'app-contact-person',
 	imports: [
-		FloatLabel,
-		InputText,
 		Button
 	],
 	templateUrl: './contact-person.html',
 	styleUrl: './contact-person.scss'
 })
 export class ContactPerson {
-
+	contact_person = signal({
+		name: '',
+		email: '',
+		phone: '',
+		position: '',
+		office_phone: ''
+	})
 }
