621df341508c86927c2661f1a63b425f21c9c647
max liu <max.liu@digitimes.com>
2025-12-01 17:45:34 +0800

    feat: enhance exhibitor rights confirmation component with display and request functionality

--- src/app/home/exhibitor-rights-confirmation/exhibitor-rights-confirmation.ts
+++ src/app/home/exhibitor-rights-confirmation/exhibitor-rights-confirmation.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Button } from "primeng/button";
 
 @Component({
-  selector: 'app-exhibitor-rights-confirmation',
-  imports: [],
-  templateUrl: './exhibitor-rights-confirmation.html',
-  styleUrl: './exhibitor-rights-confirmation.scss'
+	selector: 'app-exhibitor-rights-confirmation',
+	imports: [
+		Button
+	],
+	templateUrl: './exhibitor-rights-confirmation.html',
+	styleUrl: './exhibitor-rights-confirmation.scss'
 })
 export class ExhibitorRightsConfirmation {
-
+	data = [
+		{ itemName: 'Exhibitor Right 1', description: 'Description of Exhibitor Right 1' },
+		{ itemName: 'Exhibitor Right 2', description: 'Description of Exhibitor Right 2' },
+		{ itemName: 'Exhibitor Right 3', description: 'Description of Exhibitor Right 3' },
+	]
 }
