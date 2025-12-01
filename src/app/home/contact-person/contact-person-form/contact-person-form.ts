4fb9cba179ad441587f2ece3625c48804447430d
max liu <max.liu@digitimes.com>
2025-12-01 11:28:50 +0800

    feat: add mobile phone number fields and action buttons to contact person form

--- src/app/home/contact-person/contact-person-form/contact-person-form.ts
+++ src/app/home/contact-person/contact-person-form/contact-person-form.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { FloatLabel } from "primeng/floatlabel";
 import { InputText } from "primeng/inputtext";
+import { Button } from "primeng/button";
 
 @Component({
 	selector: 'app-contact-person-form',
 	imports: [
 		FloatLabel,
-		InputText
+		InputText,
+		Button
 	],
 	templateUrl: './contact-person-form.html',
 	styleUrl: './contact-person-form.scss'
