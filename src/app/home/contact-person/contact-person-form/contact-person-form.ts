5de0db9d9815fa0f30d4767c6cc9da0a50e18846
max liu <max.liu@digitimes.com>
2025-12-01 10:12:26 +0800

    feat: enhance contact person form with new layout and input fields for name and job title

--- src/app/home/contact-person/contact-person-form/contact-person-form.ts
+++ src/app/home/contact-person/contact-person-form/contact-person-form.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FloatLabel } from "primeng/floatlabel";
+import { InputText } from "primeng/inputtext";
 
 @Component({
-  selector: 'app-contact-person-form',
-  imports: [],
-  templateUrl: './contact-person-form.html',
-  styleUrl: './contact-person-form.scss'
+	selector: 'app-contact-person-form',
+	imports: [
+		FloatLabel,
+		InputText
+	],
+	templateUrl: './contact-person-form.html',
+	styleUrl: './contact-person-form.scss'
 })
 export class ContactPersonForm {
 
