4c8aad3bca2c2e77cc19c4a2f14664e27d00b98a
maxLiu <neoliukai@gmail.com>
2025-12-03 17:09:48 +0800

    feat: implement form structure and styling for exhibition highlights and theme components

--- src/app/home/exhibitor-information-update/exhibition-highlights/exhibition-highlights.ts
+++ src/app/home/exhibitor-information-update/exhibition-highlights/exhibition-highlights.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { FloatLabel } from "primeng/floatlabel";
+import { InputText } from "primeng/inputtext";
+import { Textarea } from "primeng/textarea";
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-exhibition-highlights',
-  imports: [],
+	imports: [
+		FloatLabel,
+		InputText,
+		Textarea,
+		Button
+	],
   templateUrl: './exhibition-highlights.html',
   styleUrl: './exhibition-highlights.scss'
 })
--- src/app/home/exhibitor-information-update/exhibition-theme/exhibition-theme.ts
+++ src/app/home/exhibitor-information-update/exhibition-theme/exhibition-theme.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { InputText } from "primeng/inputtext";
+import { FormsModule } from "@angular/forms";
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-exhibition-theme',
-  imports: [],
+	imports: [
+		InputText,
+		FormsModule,
+		Button
+	],
   templateUrl: './exhibition-theme.html',
   styleUrl: './exhibition-theme.scss'
 })
 export class ExhibitionTheme {
+	protected value = signal('「以科技連結未來」');
 
 }
