702ca737779735a907ca05047fb7e8ebecbaa0fe
max liu <max.liu@digitimes.com>
2025-12-11 17:29:56 +0800

    feat: enhance exhibitor rights confirmation with change description input and layout adjustments

--- src/app/home/exhibitor-rights-confirmation/exhibitor-rights-confirmation.ts
+++ src/app/home/exhibitor-rights-confirmation/exhibitor-rights-confirmation.ts
@@ -5,6 +5,10 @@ import { CommonDialog } from "../../shared/common-dialog/common-dialog";
 import { Checkbox } from "primeng/checkbox";
 import { FormsModule } from "@angular/forms";
 import { Tag } from "primeng/tag";
+import { FloatLabel } from "primeng/floatlabel";
+import { InputText } from "primeng/inputtext";
+import { Confirmation_of_exhibitor_right } from "../../store/home.slice";
+import { toObservable } from "@angular/core/rxjs-interop";
 
 @Component({
 	selector: 'app-exhibitor-rights-confirmation',
@@ -14,25 +18,35 @@ import { Tag } from "primeng/tag";
 		CommonDialog,
 		Checkbox,
 		FormsModule,
-		Tag
+		Tag,
+		FloatLabel,
+		InputText
 	],
 	templateUrl: './exhibitor-rights-confirmation.html',
 	styleUrl: './exhibitor-rights-confirmation.scss'
 })
 export class ExhibitorRightsConfirmation {
 	dialog = viewChild(CommonDialog)
-	data:Data[] = [
-		{ itemName: 'Exhibitor Right 1', description: 'Description of Exhibitor Right 1' },
-		{ itemName: 'Exhibitor Right 2', description: 'Description of Exhibitor Right 2' },
-		{ itemName: 'Exhibitor Right 3', description: 'Description of Exhibitor Right 3' },
+	data: Confirmation_of_exhibitor_right[] = [
+		{ itemName: '贊助權益', description: 'Description of Exhibitor Right 1', change_description: '' },
+		{ itemName: '議程場次', description: 'Description of Exhibitor Right 2', change_description: '' },
+		{ itemName: '攤位資訊', description: 'Description of Exhibitor Right 3', change_description: '' },
+		{ itemName: '不知講堂議程', description: 'Description of Exhibitor Right 4', change_description: '' },
+		{ itemName: '未來舞台議程', description: 'Description of Exhibitor Right 5', change_description: '' },
+		{ itemName: '媒體廣宣', description: 'Description of Exhibitor Right 6', change_description: '' },
 	]
-	protected selectedItemName = signal<Data[]>([]);
+	protected selectedItemName = signal<Confirmation_of_exhibitor_right[]>([]);
+
+	constructor() {
+		toObservable(this.selectedItemName).subscribe({
+			next: (selectedItems) => {
+				console.log('Selected Items:', selectedItems);
+			},
+			error: (err) => console.error('Error in selected items observable:', err)
+		})
+	}
+
 	handleClick() {
 		this.dialog()?.onOpen()
 	}
-}
-
-interface Data {
-	itemName: string;
-	description: string;
 }
--- src/app/store/home.slice.ts
+++ src/app/store/home.slice.ts
@@ -1,3 +1,33 @@
+export interface Basic_info {
+	company_name: string;
+	company_name_en: string;
+	unified_business_no: string;
+	company_website_url: string;
+	company_address: string;
+	company_profile: string;
+}
+
+export interface Contact_person {
+	name: string;
+	title: string;
+	email: string;
+	mobile_phone: string;
+	office_phone: string;
+}
+
+export interface Confirmation_of_exhibitor_right {
+	itemName: string;
+	description: string;
+	change_description?: string;
+}
+
 export interface HomeSlice {
 	data_completeness: number;
+	last_updated: string | null;
+	review_status: 'DRAFT' | 'UNDER_REVIEW' | 'APPROVED' | 'REJECTED';
+	company_unified_business_no: string | null;
+	basic_info: Basic_info;
+	contact_person: Contact_person;
+	confirmation_of_exhibitor_rights: Confirmation_of_exhibitor_right[];
+	selected_change_requests: string[];
 }
