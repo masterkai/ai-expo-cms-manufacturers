d991ba455b8b01feac0e1290fce468bae019d62a
max liu <max.liu@digitimes.com>
2025-12-12 13:41:29 +0800

    feat: add Steps and Steps_Chinese enums for navigation and update sidebar menu with localized step labels

--- src/app/home/sidebar-menu/sidebar-menu.ts
+++ src/app/home/sidebar-menu/sidebar-menu.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from "@angular/router";
 import { Ripple } from "primeng/ripple";
+import { Steps_Chinese } from "../../store/home.slice";
+import { Steps } from "primeng/steps";
 
 @Component({
 	selector: 'app-sidebar-menu',
@@ -13,5 +15,7 @@ import { Ripple } from "primeng/ripple";
 	styleUrl: './sidebar-menu.scss'
 })
 export class SidebarMenu {
-
+	steps = Steps
+	steps_Chinese = Steps_Chinese;
+	protected readonly Steps_Chinese = Steps_Chinese;
 }
--- src/app/store/home.slice.ts
+++ src/app/store/home.slice.ts
@@ -39,6 +39,26 @@ export interface Speaker_information {
 	updated_at: string;
 }
 
+export enum Steps {
+	'BASIC_INFO' = 'basic_info',
+	'CONTACT_PERSON' = 'contact_person',
+	'EXHIBITOR_RIGHTS_CONFIRMATION' = 'exhibitor_rights_confirmation',
+	'EXHIBITOR_INFORMATION_UPDATE' = 'exhibitor_information_update',
+	'SPEAKER_INFO' = 'speaker_info',
+	'FILE_DOWNLOAD' = 'file_download',
+	'REVIEW_AND_CHECK_PREVIEW_LIST' = 'review_and_check_preview_list'
+}
+
+export enum Steps_Chinese {
+	'BASIC_INFO' = '基本資料',
+	'CONTACT_PERSON' = '聯絡人',
+	'EXHIBITOR_RIGHTS_CONFIRMATION' = '參展商權益確認',
+	'EXHIBITOR_INFORMATION_UPDATE' = '參展商資料更新',
+	'SPEAKER_INFO' = '演講者資訊',
+	'FILE_DOWNLOAD' = '文件下載',
+	'REVIEW_AND_CHECK_PREVIEW_LIST' = '審核與檢視預覽清單'
+}
+
 export interface HomeSlice {
 	countdown_to_submission_deadline: string;
 	data_completeness: number;
@@ -51,6 +71,42 @@ export interface HomeSlice {
 	selected_change_requests: string[];
 	exhibition_Information_Update: Exhibition_Information_Update;
 	speaker_information: Speaker_information[];
-
-
+	proceed_to_next_step: boolean;
+	proceed_to_next_step_message: string;
 }
+
+export const initialHomeSlice: HomeSlice = {
+	countdown_to_submission_deadline: '',
+	data_completeness: 0,
+	last_updated: null,
+	review_status: 'DRAFT',
+	company_unified_business_no: null,
+	basic_info: {
+		company_name: '',
+		company_name_en: '',
+		unified_business_no: '',
+		company_website_url: '',
+		company_address: '',
+		company_profile: ''
+	},
+	contact_person: {
+		name: '',
+		title: '',
+		email: '',
+		mobile_phone: '',
+		office_phone: ''
+	},
+	confirmation_of_exhibitor_rights: [],
+	selected_change_requests: [],
+	exhibition_Information_Update: {
+		speaker_photo: new Blob(),
+		logo_image: new Blob(),
+		speech_manuscript: new Blob(),
+		briefing: new Blob(),
+		highlights: [],
+		exhibition_theme: ''
+	},
+	speaker_information: [],
+	proceed_to_next_step: false,
+	proceed_to_next_step_message: Steps.CONTACT_PERSON
+};
